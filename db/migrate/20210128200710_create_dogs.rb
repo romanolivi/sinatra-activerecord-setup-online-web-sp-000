f984a609d08b3172b89b9f500d0d69de171b8989
Romano Livi <romano_livi@outlook.com>
2021-01-28 20:16:05 +0000

    Automatically backed up by Learn

--- db/migrate/20210128200710_create_dogs.rb
+++ db/migrate/20210128200710_create_dogs.rb
@@ -11,5 +11,10 @@ class CreateDogs < ActiveRecord::Migration[5.2]
     drop_table :dogs
   end
 
-  
+  # def change 
+    # create_table :dogs do |t|
+    # t.string :name 
+    # t.string :breed 
+  # end 
+  #end
 end
